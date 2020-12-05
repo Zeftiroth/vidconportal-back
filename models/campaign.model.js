93eb8683482b2968799214bceb51f250dddc1175
Zeftiroth <zeft.huisen@gmail.com>
2020-12-06 01:52:04 +0800

    updates

---
+++ models/campaign.model.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const campaignSchema = new Schema(
+//   {
+//     title: { type: String, required: true},
+//     details: { type: String},
+//     date: { type: Date, required: true },
+//     reminders: { type: String, required: true },
+
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Campaign = mongoose.model("Campaign", compaignSchema);
+
+// module.exports = Campaign;
